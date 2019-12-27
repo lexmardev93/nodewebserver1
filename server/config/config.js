5eaf44d41ce034789c3465082b2a6cf66d7784d9
alexander <la0380022015@unab.edu.sv>
2019-12-26 18:15:46 -0600

    Variables

--- server/config/config.js
+++ server/config/config.js
@@ -12,10 +12,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev')
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-//     urlDB = 'rutamongo'
-// }
-
-process.env.URL_DB = urlDB;
+    process.env.URL_DB = urlDB;
+}
